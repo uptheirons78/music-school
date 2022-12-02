0b26f2160362a9934f156a1e3e56b4669f9269a2
mauro bono <bono.mauro@gmail.com>
2022-12-02 11:43:52 +0100

    [first-commit] Music School WP Project

---
+++ themes/music-school/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Music School functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Music School
+ * @since 1.0.0
+ */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+/**
+ * Define Constants
+ */
+define('MUSIC-SCHOOL_THEME_VERSION', '1.0.0');
+define('MUSIC-SCHOOL_THEME_DIR', get_template_directory() );
+define('MUSIC-SCHOOL_THEME_URI', get_template_directory_uri());
---
+++ themes/music-school/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Music School
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly.
+}
+
+?>
+
+<h1><?php bloginfo( 'name' ); ?></h1>

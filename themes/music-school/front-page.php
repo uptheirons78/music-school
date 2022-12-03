eb36373a7a8588dee1d1d12dcfb919eba5b71f81
mauro bono <bono.mauro@gmail.com>
2022-12-03 19:17:04 +0100

    add - front page and edit index.php

--- themes/music-school/front-page.php
+++ themes/music-school/front-page.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * The main template file.
+ * The front page template file.
  *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
@@ -23,33 +22,22 @@ get_header();
 ?>
 <section class="page-banner" style="background-image: url(<?php echo get_theme_file_uri('/assets/img/violin-player.jpg'); ?>);">
   <div class="container">
-    <?php if (is_home()) : ?>
-      <h1><?php echo __('Blog', 'music-school'); ?></h1>
-    <?php endif; ?>
+    <h1><?php bloginfo('name'); ?></h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, ad!</p>
   </div>
 </section>
 <?php if (have_posts()) : ?>
-  <section class="page-content">
-    <div class="container">
   <?php while (have_posts()) : ?>
-      <?php the_post(); ?>
+    <?php the_post(); ?>
+    <section class="page-content">
+      <div class="container">
         <article class="py-2">
           <h2><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
-          <div class="meta">
-            <p>
-              Posted by <?php the_author_posts_link(); ?> on <?php the_time('d M, Y'); ?> in <?php echo get_the_category_list(', '); ?>
-            </p>
-          </div>
-          <div class="generic-content py-2">
-            <?php the_excerpt(); ?>
-            <p><a href="<?php esc_url(the_permalink()); ?>"><?php echo __('Read more', 'music-school'); ?> &raquo;</a></p>
-          </div>
+          <?php the_content(); ?>
         </article>
+      </div>
+    </section>
   <?php endwhile; ?>
-  <?php echo paginate_links(); ?>
-    </div>
-  </section>
 <?php else : ?>
   <h2><?php echo __('There is no content to display here.', 'music-school'); ?></h2>
 <?php endif; ?>

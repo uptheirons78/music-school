ae62992cb057a3f758cd01f7fb4023e7d0ee106b
mauro bono <bono.mauro@gmail.com>
2022-12-03 15:13:43 +0100

    edit - page.php

--- themes/music-school/page.php
+++ themes/music-school/page.php
@@ -33,13 +33,45 @@ get_header();
     </section>
     <section class="page-content">
       <div class="container">
-        <ul class="breadcrumbs py-1">
-          <li><a href="#">About</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
+        <?php $parent_ID = wp_get_post_parent_id(get_the_ID()); ?>
+        <?php if ($parent_ID) : ?>
+          <ul class="breadcrumbs py-1">
+            <li>
+              <a href="<?php echo esc_url(get_the_permalink($parent_ID)); ?>">
+                <?php echo get_the_title($parent_ID); ?>
+              </a>
+            </li>
+            <li><?php the_title(); ?></li>
+          </ul>
+        <?php endif; ?>
         <div class="generic-content py-2">
           <?php the_content(); ?>
         </div>
+        <?php $pageArray = get_pages(array('child_of' => get_the_ID())); ?>
+        <?php if ($parent_ID || $pageArray) : ?>
+          <div class="page-links">
+            <h3>
+              <a href="<?php echo esc_url(get_the_permalink($parent_ID)); ?>">
+                <?php echo get_the_title($parent_ID); ?>
+              </a>
+            </h3>
+            <ul>
+              <!-- How to get a list of child pages in WordPress -->
+              <?php
+              if ($parent_ID) {
+                $childrenPages = $parent_ID;
+              } else {
+                $childrenPages = get_the_ID();
+              }
+              wp_list_pages(array(
+                'title_li'    => NULL,
+                'child_of'    => $childrenPages,
+                'sort_column' => 'menu_order'
+              ));
+              ?>
+            </ul>
+          </div>
+        <?php endif; ?>
       </div>
     </section>
   <?php endwhile; ?>

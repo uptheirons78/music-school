2ab73f8ad2255dc4a0cf42795b4465708a645571
mauro bono <bono.mauro@gmail.com>
2022-12-03 18:01:46 +0100

    edit - index.php adding the page banner

--- themes/music-school/index.php
+++ themes/music-school/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file.
  *
@@ -13,26 +14,33 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
   exit; // Exit if accessed directly.
 }
 
 get_header();
 
 ?>
-
-<div class="container">
-  <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : ?>
-      <?php the_post(); ?>
-      <article class="py-2">
-        <h2><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
-        <?php the_content(); ?>
-      </article>
-    <?php endwhile; ?>
-    <?php else : ?>
-      <h2><?php echo __('There is no content to display here.', 'music-school'); ?></h2>
-  <?php endif; ?>
-</div>
+<section class="page-banner" style="background-image: url(<?php echo get_theme_file_uri('/assets/img/violin-player.jpg'); ?>);">
+  <div class="container">
+    <h1>Music School</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, ad!</p>
+  </div>
+</section>
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : ?>
+    <?php the_post(); ?>
+    <section class="page-content">
+      <div class="container">
+        <article class="py-2">
+          <h2><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
+          <?php the_content(); ?>
+        </article>
+      </div>
+    </section>
+  <?php endwhile; ?>
+<?php else : ?>
+  <h2><?php echo __('There is no content to display here.', 'music-school'); ?></h2>
+<?php endif; ?>
 
 <?php get_footer(); ?>

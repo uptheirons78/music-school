5dc00aa24881ed8cf9d57ba31ececcf5fc3d8469
mauro bono <bono.mauro@gmail.com>
2022-12-06 22:08:33 +0100

    add - past events page with custom queries to retrieve all past events

--- themes/music-school/page-past-events.php
+++ themes/music-school/page-past-events.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The template for displaying all events.
+ * The template for the page: Past Events.
  *
  * @package Music School
  * @since 1.0.0
@@ -16,15 +16,36 @@ get_header();
 ?>
 <section class="page-banner" style="background-image: linear-gradient(to bottom, rgba(0,0,0,.3), rgba(0,0,0,.9)), url(<?php echo get_theme_file_uri('/assets/img/rock-event.jpg'); ?>);">
   <div class="container">
-    <h1><?php _e('All Our Events', 'music-school'); ?></h1>
-    <p><?php _e('See what is going on in our music school.', 'music-school'); ?></p>
+    <h1><?php _e('Past Events', 'music-school'); ?></h1>
+    <p><?php _e('Take a look at our past events archive.', 'music-school'); ?></p>
   </div>
 </section>
-<?php if (have_posts()) : ?>
+<?php
+$today = date('Ymd');
+
+$meta_query_args = array(
+  'key'     => 'event_date',
+  'compare' => '<',
+  'value'   => $today,
+  'type'    => 'numeric'
+);
+
+$past_events_query_args = array(
+  'paged'           => get_query_var('paged', 1),
+  'post_type'       => 'event',
+  'meta_key'        => 'event_date',
+  'orderby'         => 'meta_value_num',
+  'order'           => 'DESC',
+  'meta_query'      => array($meta_query_args)
+);
+
+$pastEvents = new WP_Query($past_events_query_args);
+?>
+<?php if ($pastEvents->have_posts()) : ?>
   <section class="page-content">
     <div class="container">
-      <?php while (have_posts()) : ?>
-        <?php the_post(); ?>
+      <?php while ($pastEvents->have_posts()) : ?>
+        <?php $pastEvents->the_post(); ?>
         <article class="py-2">
           <h2><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
           <div class="meta">
@@ -38,13 +59,16 @@ get_header();
           </div>
         </article>
       <?php endwhile; ?>
-      <?php echo paginate_links(); ?>
-      <hr>
-      <p class="py-2"><?php echo __('Take a look at our', 'music-school'); ?> <a href="<?php echo site_url('/past-events'); ?>"><?php echo __('past events', 'music-school'); ?> &raquo;</a></p>
+      <?php
+        echo paginate_links(array(
+          'total' => $pastEvents->max_num_pages
+        ));
+      ?>
     </div>
   </section>
 <?php else : ?>
   <?php get_template_part('template-parts/content', 'none'); ?>
 <?php endif; ?>
+<?php wp_reset_postdata(); ?>
 
 <?php get_footer(); ?>
